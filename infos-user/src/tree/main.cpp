7e2f1195035a8da24417eec1801d5de4b6cc5bb9
arakban <arka.purkayastha10@gmail.com>
2022-04-04 09:20:11 +0100

    start is end

--- infos-user/src/tree/main.cpp
+++ infos-user/src/tree/main.cpp
@@ -3,10 +3,43 @@
  * SKELETON IMPLEMENTATION TO BE FILLED IN FOR TASK 3
  */
 
+
 #include <infos.h>
+
+bool isEnd()
+
 
 int main(const char *cmdline)
 {
-    // TODO: Implement me!
-    return 0;
+	const char *path;
+	char next_dir[256];
+	bool end;
+	int *ptr; 
+	
+	//keep count of total no. of files and directories, and tree structure
+	int depth = 0;
+	int n_files = 0;
+	int n_dir = 0; 
+
+	if (!cmdline || strlen(cmdline) == 0) {
+		path = "/usr";
+	} else {
+		path = cmdline;
+	}
+	
+	HDIR cur_dir = opendir(path, 0);
+	if (is_error(cur_dir)) {
+		printf("Unable to open current top directory '%s' for reading.\n", path);
+		return 1;
+	}
+	
+	printf("Directory Listing of '%s':\n", path);
+	
+	end = false;
+	
+	strcpy(next_dir,path);
+	printf(".\n");
+	parse_dir(next_dir, depth, end, ptr);
+
+	return 0;
 }
